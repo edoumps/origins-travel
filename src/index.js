5772c79a0c4041ef59cf667ed7c9f7c12cd32b2a
Percy <edoumps@gmail.com>
2022-04-29 10:34:47 +0100

    Just added accouple files modified (.json files, index.js and template.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import generateJoke from './App'
 import './styles/main.scss'
+import emoLaugh from './assets/emoLaugh.svg'
+
+const emoImg = document.getElementById('emoImg')
+emoImg.src = emoLaugh
 
 console.log(generateJoke())
 console.log('Yes Man')
